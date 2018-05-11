4302ed262dd25172cf6d9edc619760c470041fef
Vipul Sharma <vipulsharma018@gmail.com>
2018-05-11 14:58:00 -0400

    Assignment #4

--- 1 Bootstrap/Week 4/README.md
+++
@@ -1,8 +0,0 @@
-# NPM SCRIPTS
-
-"scripts" property in the `package.json` file:
-
-* Several supported
-* `start`: run by the npm start command
-* Of particular interests to us is to use the `npm run` approach to run scripts
-* eg, `npm run scss`, `npm run lite`
--- 1 Bootstrap/Week 4/js/scripts.js
+++ 1 Bootstrap/Week 4/js/scripts.js
@@ -1,41 +1,27 @@
-$(document).ready(function()
-        {
-            $('#myCarousel').carousel({interval: 2000});
-            $('#carouselButton').click(function()
-            {
-                if ($('#carouselButton').children('span').hasClass('fa-pause'))
-                {
-                    $('#myCarousel').carousel('pause');
-                    $('#carouselButton').children('span').removeClass('fa-pause');
-                    $('#carouselButton').children('span').addClass('fa-play');
-                }
-                else if ($('#carouselButton').children('span').hasClass('fa-play'))
-                {
-                    $('#myCarousel').carousel('cycle');
-                    $('#carouselButton').children('span').removeClass('fa-play');
-                    $('#carouselButton').children('span').addClass('fa-pause');
-                }
-            });
+$(document).ready(function(){
+    $("#mycarousel").carousel({interval: 2000});
+    $("#carousel-button").click(function(){
+        if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass('fa-pause');
+            $("#carousel-button").children("span").addClass('fa-play');
+        }
+        else if ($("#carousel-button").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass('fa-play');
+            $("#carousel-button").children("span").addClass('fa-pause');                    
+        }
+    });
+});
 
-            // Reservation Modal JQuery code
-            $('#reservationButton').click(function(){
-              $('#reserveTableModal').modal('show');
-            });
-            $('#reservationCancelButton').click(function(){
-              $('#reserveTableModal').modal('hide');
-            });
-            $('#reservationCloseButton').click(function(){
-              $('#reserveTableModal').modal('hide');
-            });
+$(document).ready(function(){
+    $("#Myreservetable").click(function(){
+        $("#reservetable").modal();
+    });
+});
 
-            // Login Modal JQuery code
-            $('#loginButton').click(function() {
-              $('#loginModal').modal('show');
-            });
-            $('#loginCancelButton').click(function() {
-              $('#loginModal').modal('hide');
-            });
-            $('#loginCloseButton').click(function() {
-              $('#loginModal').modal('hide');
-            });
-        })
+$(document).ready(function(){
+    $("#loginBtn").click(function(){
+        $("#loginModal").modal();
+    });
+});
