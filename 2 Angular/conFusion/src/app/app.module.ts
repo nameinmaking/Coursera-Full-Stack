dcebd1220f880a66b2a4bcff7a3214061c9d116c
Vipul Sharma <vipulsharma018@gmail.com>
2018-07-09 16:10:43 -0400

    Configuring Angular

--- 2 Angular/conFusion/src/app/app.module.ts
+++ 2 Angular/conFusion/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
+// import {HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
+// import {MaterialModule} from '@angular/material';
+// Since Material Module has been deprecated we are using it indirectly from the below file
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    // HttpModule,  Deprecated
+    MatToolbarModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
